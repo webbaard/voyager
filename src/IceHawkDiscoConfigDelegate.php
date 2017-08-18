7192a681e6c3a69df5991487743401e3eca2fcc3
Tim Huijzers <Tim.huijzers@drukwerkdeal.nl>
2017-08-18 14:34:12 +0200

    disco-config

---
+++ src/IceHawkDiscoConfigDelegate.php
@@ -0,0 +1,66 @@
+<?php
+declare(strict_types = 1);
+
+namespace Printdeal\Voyager;
+
+use bitExpert\Disco\BeanFactory;
+use IceHawk\IceHawk\Interfaces\ConfiguresIceHawk;
+use IceHawk\IceHawk\Interfaces\ProvidesCookieData;
+use IceHawk\IceHawk\Interfaces\ProvidesRequestInfo;
+use IceHawk\IceHawk\Interfaces\RespondsFinallyToReadRequest;
+use IceHawk\IceHawk\Interfaces\RespondsFinallyToWriteRequest;
+use IceHawk\IceHawk\Routing\Interfaces\BypassesRequest;
+
+/**
+ * Class IceHawkDiscoConfigDelegate
+ *
+ * @package Printdeal\Voyager
+ */
+final class IceHawkDiscoConfigDelegate implements ConfiguresIceHawk
+{
+    protected $beanFactory;
+
+    /**
+     * Creates a new {@link \PrintDeal\Voyager\IceHawkDiscoConfigDelegate}.
+     *
+     * @param BeanFactory $beanFactory
+     */
+    public function __construct(BeanFactory $beanFactory)
+    {
+        $this->beanFactory = $beanFactory;
+    }
+    public function getRequestInfo() : ProvidesRequestInfo
+    {
+        return $this->beanFactory->get('requestInfo');
+    }
+    public function getReadRoutes() : array
+    {
+        return $this->beanFactory->get('readRoutes');
+    }
+    public function getWriteRoutes() : array
+    {
+        return $this->beanFactory->get('writeRoutes');
+    }
+    public function getEventSubscribers() : array
+    {
+        return $this->beanFactory->get('eventSubscribers');
+    }
+    public function getFinalReadResponder() : RespondsFinallyToReadRequest
+    {
+        return $this->beanFactory->get('finalReadResponder');
+    }
+    public function getFinalWriteResponder() : RespondsFinallyToWriteRequest
+    {
+        return $this->beanFactory->get('finalWriteResponder');
+    }
+
+    public function getRequestBypasses(): BypassesRequest
+    {
+        return $this->beanFactory->get('getRequestBypasses');
+    }
+
+    public function getCookies(): ProvidesCookieData
+    {
+        return $this->beanFactory->get('getCookies');
+    }
+}
